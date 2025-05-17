a0664b6e3f347dfd5a72b80e3556ff6d3188c101
Arina <arinapereteatcu0@gmail.com>
2025-05-17 14:06:47 +0300

    First commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using DatingApp.Data;
+using DatingApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Controllers;
+[ApiController]
+[Route("[controller]")] //api/users
+
+public class UsersController(DataContext context) : ControllerBase
+{
+
+    [HttpGet]
+    public async Task <ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await context.Users.ToListAsync();
+        return Ok(users);
+    }
+    [HttpGet("{id:int}")] //api/users/1
+    public async Task< ActionResult<AppUser>> GetUser(int id)
+    {
+        var user = await context.Users.FindAsync(id);
+        if (user == null) return NotFound();
+        return user;
+    }
+}
